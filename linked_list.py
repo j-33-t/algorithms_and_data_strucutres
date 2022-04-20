0961b6bd6ea6d8de12f797db988f49fb27684dca
j-33-t <prasenjeet.rathore@gmail.com>
2022-04-20 18:58:57 +0200

    20-04-2022

--- linked_list.py
+++ linked_list.py
@@ -1,4 +1,3 @@
-from this import d
 
 
 class Node:
@@ -26,3 +25,47 @@ class LinkedList:
         
     def is_empty(self):
         return self.head == None
+    
+    def size(self):
+        """
+        Returns the number of nodes in the list 
+        Takes 0(n) time
+        """
+        
+        current = self.head
+        count = 0
+        
+        while current:
+            count = count + 1
+            current = current.next_node
+            
+        return count
+    
+    def add(self, data):
+        """
+        Adds new Node containing data at head of the list
+        Takes 0(1) time
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+        
+    def __repr__(self):
+        """
+        Returns a string representation of the list
+        Takes 0(n) time
+        """
+        
+        nodes = []
+        current = self.head
+        
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+                
+            current = current.next_node
+        return '->'.join(nodes)
